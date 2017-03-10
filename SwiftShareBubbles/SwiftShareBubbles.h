479fdcbfb9615ca1daffa2703c991e8e9745e9e0
Fujiki Takeshi <jp21265@w022461607b28m.local>
2017-03-10 17:19:18 +0900

    create project

---
+++ SwiftShareBubbles/SwiftShareBubbles.h
@@ -0,0 +1,19 @@
+//
+//  SwiftShareBubbles.h
+//  SwiftShareBubbles
+//
+//  Created by Fujiki Takeshi on 2017/03/10.
+//  Copyright © 2017年 com.takecian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftShareBubbles.
+FOUNDATION_EXPORT double SwiftShareBubblesVersionNumber;
+
+//! Project version string for SwiftShareBubbles.
+FOUNDATION_EXPORT const unsigned char SwiftShareBubblesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftShareBubbles/PublicHeader.h>
+
+
---
+++ SwiftShareBubblesTests/SwiftShareBubblesTests.swift
@@ -0,0 +1,36 @@
+//
+//  SwiftShareBubblesTests.swift
+//  SwiftShareBubblesTests
+//
+//  Created by Fujiki Takeshi on 2017/03/10.
+//  Copyright © 2017年 com.takecian. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftShareBubbles
+
+class SwiftShareBubblesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
