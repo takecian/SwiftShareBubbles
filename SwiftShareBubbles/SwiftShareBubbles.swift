889984c6c4ae8a5d03078eabe0077e6736533647
Fujiki Takeshi <jp21265@w022461607b28m.local>
2017-03-10 17:32:16 +0900

    relocate files

---
+++ SwiftShareBubbles/SwiftShareBubbles.swift
@@ -0,0 +1,13 @@
+//
+//  SwiftShareBubbles.swift
+//  SwiftShareBubbles
+//
+//  Created by Fujiki Takeshi on 2017/03/10.
+//  Copyright © 2017年 com.takecian. All rights reserved.
+//
+
+import UIKit
+
+public class SwiftShareBubbles {
+
+}
