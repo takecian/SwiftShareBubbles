3cedcb3c00fe67e6f4bb400be1b1b2b8af1f6a3c
takecian <fujikidev@gmail.com>
2017-03-14 11:15:42 +0900

    [WIP] Custom social buttons

--- Demo/Demo/ViewController.swift
+++ Demo/Demo/ViewController.swift
@@ -13,11 +13,16 @@ import Social
 class ViewController: UIViewController, SwiftShareBubblesDelegate {
 
     var bubbles: SwiftShareBubbles?
+    let customBubbleId = 100
     
     override func viewDidLoad() {
         super.viewDidLoad()
         bubbles = SwiftShareBubbles(point: CGPoint(x: view.frame.width / 2, y: view.frame.height / 2), radius: 100, in: view)
         bubbles?.showBubbleTypes = [Bubble.twitter, Bubble.line, Bubble.safari]
+        
+        let customAttribute = ShareAttirbute(bubbleId: 100, icon: UIImage(named: "")!, backgroundColor: UIColor.blue)
+        bubbles?.customBubbleAttributes = [customAttribute]
+        
         bubbles?.delegate = self
     }
 
